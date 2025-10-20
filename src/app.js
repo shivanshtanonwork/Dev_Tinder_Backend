de39357b6b44661a850238980d88155d76efa002
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-20 13:22:00 +0530

    req.params

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
-  console.log(req.query);
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Shivansh", lastName: "Tandon" });
 });
 
