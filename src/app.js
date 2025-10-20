06af1f6a66774fc8991a484813523c20a219164f
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-20 13:20:31 +0530

    req.query

--- src/app.js
+++ src/app.js
@@ -1,22 +1,9 @@
 const express = require("express");
 const app = express();
 
-// This will only handle GET call to /user
 app.get("/user", (req, res) => {
+  console.log(req.query);
   res.send({ firstName: "Shivansh", lastName: "Tandon" });
-});
-
-app.post("/user", (req, res) => {
-  console.log("Save data to database");
-  res.send("Data successfully saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
-// This will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hi from server");
 });
 
 app.listen(7777, () => {
