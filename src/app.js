e3c56b3be37ff8e3ad1df22b8bc52f6a2484bf11
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-20 13:03:41 +0530

    Exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Shivansh", lastName: "Tandon" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save data to database");
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+// This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("Hi from server");
 });
 
-app.listen(3000, () => {
+app.listen(7777, () => {
   console.log("Sever is successfully listening on port 3000...");
 });
